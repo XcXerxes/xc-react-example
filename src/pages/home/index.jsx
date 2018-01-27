54d210392c0e6907db8a5dba4536c165a0b7f25e
Smile <xcxerxes@aliyun.com>
2018-01-27 18:37:17 +0800

    home inpput

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,6 +8,9 @@ class Home extends Component {
     super(props)
     this.state = {}
   }
+  handleInput = (type, event) => {
+    this.props.saveFormData(event.target.value, type)
+  }
   render () {
     console.log(this.props)
     const headerInfo = {title: '首页', isRight: true}
@@ -23,19 +26,21 @@ class Home extends Component {
               <div className="home-form__content">
                 <div className="home-form__item">
                   <span>销售金额：</span>
-                  <input placeholder="请输入金额" className="home-form__input input-noLine" />
+                  <input placeholder="请输入金额" type="number" value={this.props.formData.orderSum}  pattern="[0-9]*" className="home-form__input input-noLine" onChange={this.handleInput.bind(this, 'orderSum')} />
                 </div>
                 <div className="home-form__item">
                   <span>客户姓名：</span>
-                  <input placeholder="请输入姓名" className="home-form__input input-noLine" />
+                  <input placeholder="请输入姓名" value={this.props.formData.name} className="home-form__input input-noLine" onChange={this.handleInput.bind(this, 'name')} />
                 </div>
                 <div className="home-form__item">
                 <span>客户电话：</span>
-                  <input placeholder="请输入电话"  className="home-form__input input-noLine" />
+                  <input placeholder="请输入电话" type="tel"  value={this.props.formData.phoneNo} className="home-form__input input-noLine" onChange={this.handleInput.bind(this, 'phoneNo')} />
                 </div>
               </div>
             </div>
-            <div className="home-form__footer"></div>
+            <div className="home-form__footer">
+              <button className="btn-default home-btn__submit">提交</button>
+            </div>
           </div>
         </div>
       </main>
